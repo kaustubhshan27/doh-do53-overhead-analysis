12dbd59a38679aa93cd76c377daec5dda99f427b
Kaustubh Shankar <kaustubhshan@gmail.com>
2024-04-12 19:30:11 -0400

    doh persistent tested and results added

--- code/doh_analysis.py
+++ code/doh_analysis.py
@@ -3,13 +3,14 @@
 import csv
 import dns.query
 import dns.message
+import httpx
 
 # Define the DoH endpoints for Google and Cloudflare
 GOOGLE_DNS_HTTPS = 'https://dns.google/dns-query'               # https://developers.google.com/speed/public-dns/docs/doh
 CLOUDFLARE_DNS_HTTPS = 'https://cloudflare-dns.com/dns-query'   # https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests/
 
 # CSV file path
-CSV_FILE_PATH = 'top-domains-db/test_domains.csv'
+CSV_FILE_PATH = '../top-domains-db/test_domains.csv'
 
 def resolve_doh(dns_service_url, session=None):
     with open(CSV_FILE_PATH, newline='') as csvfile:
@@ -24,19 +25,19 @@ def resolve_doh(dns_service_url, session=None):
 
 def main():
     # Without persistent connections
-    print("\nWithout persistent connections:")
+    # print("\nWithout persistent connections:")
     # print("Resolving with Google DNS...")
     # resolve_doh(GOOGLE_DNS_HTTPS)
-    print("Resolving with Cloudflare DNS...")
-    resolve_doh(CLOUDFLARE_DNS_HTTPS)
+    # print("Resolving with Cloudflare DNS...")
+    # resolve_doh(CLOUDFLARE_DNS_HTTPS)
 
     # Using persistent connections
-    # print("Using persistent connections:")
-    # with dns.query.httpsession() as session:
-    #     print("Resolving with Google DNS...")
-    #     resolve_doh(GOOGLE_DNS_HTTPS, session=session)
-    #     print("Resolving with Cloudflare DNS...")
-    #     resolve_doh(CLOUDFLARE_DNS_HTTPS, CLOUDFLARE_DNS_HTTPS, session=session)
+    print("Using persistent connections:")
+    with httpx.Client() as client:
+        #print("Resolving with Google DNS...")
+        #resolve_doh(GOOGLE_DNS_HTTPS, session=client)
+        print("Resolving with Cloudflare DNS...")
+        resolve_doh(CLOUDFLARE_DNS_HTTPS, session=client)
 
 if __name__ == "__main__":
     main()
